12404f82d1c8b98ee652c0429ad9e18f3fe609e5
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2020-12-13 09:24:53 -0600

    Add admin dashboard

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,6 @@
+# Class DashboardController
+module Admin
+  class DashboardController < ApplicationController
+    def index; end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'dashboard#index'
+  end
+
   # api routes
   get '/api/i18n/:locale' => 'api#i18n'
 
