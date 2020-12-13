2de1252e4a9cd757baa1f0a16687834eca673547
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2020-12-12 21:11:30 -0600

    Add bootstrap and home controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+require("stylesheets/application.scss")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
