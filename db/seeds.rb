155ab5c695588a951c79bcade35f24f4745f6c70
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-02-13 01:10:54 -0600

    add csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Dir[File.join(Rails.root, 'db', 'seeds', 'catalogs', '*.rb')].sort.each do |seed
   load seed
   print "DONE\n"
 end
+
+require 'csv'
+filename = "#{Rails.root}/db/seeds/catalogs/regions.csv"
+
+
+CSV.foreach(filename, liberal_parsing: true, headers: :first_row) do |row|
+  puts row
+end
