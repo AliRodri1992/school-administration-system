f7ac874bd954cf41ba987f9197c8514d858fa944
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-02-12 18:49:31 -0600

    add universites model

---
+++ db/migrate/20210213004913_create_universities.rb
@@ -0,0 +1,10 @@
+class CreateUniversities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :universities do |t|
+      t.string :name
+      t.string :motto
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/universities.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :university do
+    name { "MyString" }
+    motto { "MyString" }
+  end
+end
