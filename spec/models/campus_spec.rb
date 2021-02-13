67d0029952d89dc19a1acb8135d411e6930d3a5c
Ivan Ali Rodriguez Vasquez <ali.rodri.vasquez@gmail.com>
2021-02-12 20:31:29 -0600

    add tests for campus

--- spec/models/campus_spec.rb
+++ spec/models/campus_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Campus, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @campus1 = create(:campus)
+  end
+
+  it 'is valid with valid attributes' do
+    expect(@campus1).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    campus2 = build(:campus, name: nil)
+    expect(campus2).to_not be_valid
+  end
+
+  it "has a unique name" do
+    campus2 = create(:campus, name: "Example")
+    campus3 = build(:campus, name: 'Example')
+    expect(campus3).to_not be_valid
+  end
 end
